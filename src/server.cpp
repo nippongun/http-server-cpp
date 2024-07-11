a4eb2c5ddc1db059cf8c14d16024174591fe06b7
simppa <simppabauer@gmail.com>
2024-07-11 21:17:48 +0300

    test stage 6

--- src/server.cpp
+++ src/server.cpp
@@ -264,6 +264,13 @@ int main(int argc, char **argv)
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  string dir;
+
+  if (argc == 3 && strcmp(argv[1], "--directory") == 0)
+  {
+    dir = argv[2];
+  }
+
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
