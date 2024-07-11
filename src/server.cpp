3ea7ce07abbc650ea9698672911f33ab260dfa8d
simppa <simppabauer@gmail.com>
2024-07-11 14:53:27 +0300

    Refactor wrappers

--- src/server.cpp
+++ src/server.cpp
@@ -102,12 +102,34 @@ public:
   }
   void parse(HTTPResponse &response, HTTPRequest &request) override
   {
-    string key = "User-Agent: ";
-    auto echo = request.tokens[2].substr(key.size());
-    cout << "UserAgent: " << echo << endl;
-    response.addHeader("Content-Type", "text/plain");
-    response.addHeader("Content-Length", std::to_string(echo.length()));
-    response.addBody(echo);
+    try
+    {
+
+      if (request.tokens.size() < 3)
+      {
+        throw std::invalid_argument("Not enough tokens in the request");
+      }
+
+      const std::string key = "User-Agent: ";
+      const std::string &userAgentHeader = request.tokens[2];
+
+      if (userAgentHeader.substr(0, key.size()) != key)
+      {
+        throw std::invalid_argument("Invalid User-Agent header format");
+      }
+
+      auto echo = request.tokens[2].substr(key.size());
+
+      cout << "UserAgent: " << echo << endl;
+      response.addHeader("Content-Type", "text/plain");
+      response.addHeader("Content-Length", std::to_string(echo.length()));
+      response.addBody(echo);
+    }
+    catch (const exception &e)
+    {
+      response.setStatusCode(400);
+      response.addBody("Bad Request: " + std::string(e.what()));
+    }
   }
 };
 
